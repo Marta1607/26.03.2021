fe6c1fb402ebb355cc545241623aee17314eb648
Marta1706 <marta.kar.zuber@gmail.com>
2021-03-26 10:56:51 +0100

    jaro

---
+++ maim.js
@@ -0,0 +1,23 @@
+// function getValue(){
+//     const inp = document.getElementsByClassName('inp')
+//     const tablica = Array.from(inp)
+//     console.log(tablica)
+// }
+
+function pobieranie() {
+    let imie = document.getElementById('imie').value
+    let nazwisko = document.getElementById('nazwisko').value
+    let email = document.getElementById('email').value
+    let telefon = document.getElementById('telefon').value
+    let wiadomość = document.getElementById('wiadomość').value
+    console.log(imie, nazwisko, email, telefon, wiadomość)
+}
+
+function myFunction() {
+    const imie = document.getElementById('imie')
+    const nazwisko = document.getElementById('nazwisko')
+    const email = document.getElementById('email')
+    const telefon = document.getElementById('telefon')
+    const wiadomość = document.getElementById('wiadomość')
+    console.log(imie, nazwisko, email, telefon, wiadomość)
+}
